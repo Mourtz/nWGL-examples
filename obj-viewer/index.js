a17169331859fb1eea02edfa7c13e1226974cd2b
Alex Mourtziapis <alex.mourtziapis@gmail.com>
2019-08-17 00:14:54 +0300

    Implement right click zoom feature

--- obj-viewer/index.js
+++ obj-viewer/index.js
@@ -75,21 +75,34 @@ document.addEventListener('mousedown', function(event){;
     mouse_down = true;
 });
 
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+}, false);
+
 document.addEventListener('mousemove', function(event){
     if(!mouse_down) return;
 
     let delta = [event.x - click_pos[0], event.y - click_pos[1]];
     click_pos = [event.x, event.y];
-    
-    delta[0] = nWGL.helper.degToRad(delta[0]*0.08);
-    delta[1] = nWGL.helper.degToRad(delta[1]*0.08);
 
-    rotation[1] += delta[0];
-    // rotation[0] += delta[1];
-            
     matrix = nWGL.helper.translate(matrix, -translation[0], -translation[1], -translation[2]);
-    matrix = nWGL.helper.yRotate(matrix, delta[0]);
-    // matrix = nWGL.helper.xRotate(matrix, delta[1]);
+    if(event.which === 3){
+        temp1 = Math.sin(rotation[1])*delta[1]*0.008;
+        temp2 = Math.cos(rotation[1])*delta[1]*0.008;
+
+        translation[0] -= temp1;
+        translation[2] += temp2;
+        matrix = nWGL.helper.translate(matrix, -temp1, 0, temp2);
+    } else {
+        delta[0] = nWGL.helper.degToRad(delta[0]*0.08);
+        delta[1] = nWGL.helper.degToRad(delta[1]*0.08);
+    
+        rotation[1] += delta[0];
+        // rotation[0] += delta[1];
+                
+        matrix = nWGL.helper.yRotate(matrix, delta[0]);
+        // matrix = nWGL.helper.xRotate(matrix, delta[1]);
+    }
     matrix = nWGL.helper.translate(matrix, translation[0], translation[1], translation[2]);
 
     sandbox.programs["display"].addUniform("u_matrix", "Matrix4fv", matrix);
