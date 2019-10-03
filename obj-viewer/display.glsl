803cd916b508d8a42b7a88834f541985dfbf9a96
Alex Mourtziapis <alex.mourtziapis@gmail.com>
2019-10-03 16:45:33 +0300

    Display the absolute value of the normal

--- obj-viewer/display.glsl
+++ obj-viewer/display.glsl
@@ -10,5 +10,5 @@ flat in vec3 v_normal;
 const vec3 light_dir = normalize(vec3(0.0, 0.0, 1.0));
 void main() {
 	// FragColor = vec4(vec3(dot(v_normal,light_dir)), 1.0);
-	FragColor = vec4(vec3(v_normal), 1.0);
+	FragColor = vec4(abs(v_normal), 1.0);
 }
